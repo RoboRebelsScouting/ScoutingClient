6c4ac42208266969101d89c523934f401641d485
RoboRebelsScouting <Administrator@WHS-ROBOTICS-02.wps.lan>
2017-03-30 21:15:23 -0400

    thursday
    file is sending

--- ScoutingClient/app/src/main/java/com/example/administrator/scoutingclient/BluetoothClient.java
+++ ScoutingClient/app/src/main/java/com/example/administrator/scoutingclient/BluetoothClient.java
@@ -12,10 +12,14 @@ import android.util.Log;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 /**
- * Created by Administrator on 3/29/2017.
+ * Created by Administrator on 3/29/2017
  */
 
 public class BluetoothClient extends Thread {
@@ -33,8 +37,6 @@ public class BluetoothClient extends Thread {
         public static final int MESSAGE_READ = 0;
         public static final int MESSAGE_WRITE = 1;
         public static final int MESSAGE_TOAST = 2;
-
-        // ... (Add other message types here as needed.)
     }
     private byte[] mmBuffer;
 
@@ -74,27 +76,89 @@ public class BluetoothClient extends Thread {
 
     }
 
-    public void btSend(String message) {
-        byte[] bytes = message.getBytes();
+    // convert little endian to big endian
+    int littleToBig(int i)
+    {
+        int b0,b1,b2,b3;
 
+        b0 = (i&0x000000ff)>>0;
+        b1 = (i&0x0000ff00)>>8;
+        b2 = (i&0x00ff0000)>>16;
+        b3 = (i&0xff000000)>>24;
+
+        return ((b0<<24)|(b1<<16)|(b2<<8)|(b3<<0));
+    }
+
+    // convert an integer to a byte array
+    public byte[] intToBytes( final int i ) {
+        ByteBuffer bb = ByteBuffer.allocate(4);
+        bb.putInt(i);
+        return bb.array();
+    }
+
+    public void btSend(String message) {
+        short msgType = 1;
+        byte[] header = new byte[2];
+        //header[0] = (byte) (msgType & 0xff);
+        //header[1] = (byte) ((msgType >> 8) & 0xff);
+        header[0] = 0;
+        header[1] = 1;
+
+        String fname = String.format("%50s","UNH-1-1153-Jon");
+        byte[] fileName = new byte[50];
+        fileName = fname.getBytes();
+
+
+
+        byte[] messagebytes = message.getBytes();
+
+        Integer fLength = messagebytes.length;
+        byte[] fileLength = new byte[4];
+        fileLength = intToBytes(fLength);
+
+        byte[] checksumbytes = new byte[20];
         try {
-            mmOutStream.write(bytes);
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            md.update(messagebytes);
+            checksumbytes = md.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+
+        /*byte[] bytes = new byte[header.length+checksumbytes.length+messagebytes.length];
+
+        for(int i = 0; i < messagebytes.length; i++) {
+            bytes[i] = messagebytes[i];
+        }
+        for (int i = 0; i < checksumbytes.length; i++) {
+            bytes[i+messagebytes.length] = checksumbytes[i];
+        }
+        for(int i = 0; i < header.length; i++) {
+            bytes[i+checksumbytes.length+messagebytes.length] = header[i];
+        }*/
+        try {
+            mmOutStream.write(header);
+            mmOutStream.write(checksumbytes);
+            mmOutStream.write(fileName);
+            mmOutStream.write(fileLength);
+            mmOutStream.write(messagebytes);
 
             // Share the sent message with the UI activity.
-            Message writtenMsg = mHandler.obtainMessage(
-                    MessageConstants.MESSAGE_WRITE, -1, -1, mmBuffer);
-            writtenMsg.sendToTarget();
+            //Message writtenMsg = mHandler.obtainMessage(
+            //        MessageConstants.MESSAGE_WRITE, -1, -1, mmBuffer);
+            //writtenMsg.sendToTarget();
         } catch (IOException e) {
             Log.e(TAG, "Error occurred when sending data", e);
 
             // Send a failure message back to the activity.
-            Message writeErrorMsg =
-                    mHandler.obtainMessage(MessageConstants.MESSAGE_TOAST);
-            Bundle bundle = new Bundle();
-            bundle.putString("toast",
-                    "Couldn't send data to the other device");
-            writeErrorMsg.setData(bundle);
-            mHandler.sendMessage(writeErrorMsg);
+            //Message writeErrorMsg =
+            //        mHandler.obtainMessage(MessageConstants.MESSAGE_TOAST);
+           // Bundle bundle = new Bundle();
+           // bundle.putString("toast",
+           //         "Couldn't send data to the other device");
+            //writeErrorMsg.setData(bundle);
+            //mHandler.sendMessage(writeErrorMsg);
         }
     }
 
@@ -106,6 +170,7 @@ public class BluetoothClient extends Thread {
             // Connect to the remote device through the socket. This call blocks
             // until it succeeds or throws an exception.
             mmSocket.connect();
+            btSend(MainActivity.messageString);
         } catch (IOException connectException) {
             // Unable to connect; close the socket and return.
             try {
@@ -118,7 +183,7 @@ public class BluetoothClient extends Thread {
 
         // The connection attempt succeeded. Perform work associated with
         // the connection in a separate thread.
-       // manageMyConnectedSocket(mmSocket);
+        // manageMyConnectedSocket(mmSocket);
 
 
 
--- ScoutingClient/app/src/main/java/com/example/administrator/scoutingclient/MainActivity.java
+++ ScoutingClient/app/src/main/java/com/example/administrator/scoutingclient/MainActivity.java
@@ -6,22 +6,24 @@ import android.content.Intent;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    public String messageString = "hi there";
+    public static String messageString = "here we go";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
     public void init() {
         int REQUEST_ENABLE_BT = 1;
         BluetoothAdapter mBluetoothAdapter = android.bluetooth.BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
-            // Device does not support Bluetooth
+            Toast.makeText(getApplicationContext(), "No Bluetooth", Toast.LENGTH_LONG).show();
         }
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(android.bluetooth.BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -37,10 +39,11 @@ public class MainActivity extends AppCompatActivity {
                 String deviceName = device.getName();
                 String deviceHardwareAddress = device.getAddress(); // MAC address
 
-                if (deviceName.equalsIgnoreCase("scoutingserver")) {
+                if (deviceName.equalsIgnoreCase("Blue3")) {
                     // do stuff
                     BluetoothClient btClient = new BluetoothClient(mBluetoothAdapter,device);
-                    btClient.btSend(messageString);
+                    btClient.start();
+                    //btClient.btSend(messageString);
                 }
             }
         }
